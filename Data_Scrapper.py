eac006663dc13e1f8840a9016afd070dfec008ba
thetom061 <tcotte061@gmail.com>
2022-02-15 22:20:08 +0100

    Function to extract lyrics out of page manages to find every verse in a page

--- Data_Scrapper.py
+++ Data_Scrapper.py
@@ -1,15 +1,32 @@
-from cgitb import html
 import requests
-import os
 from bs4 import BeautifulSoup
+import re
 
+# * Artist Page
 default_page = "https://www.azlyrics.com/w/west.html"
 
+# * Example page
+page = "https://www.azlyrics.com/lyrics/kanyewest/gorgeous.html"
 
-page = "https://www.azlyrics.com/lyrics/kanyewest/wedontcare.html"
-html_text = requests.get(page).content
-soup = BeautifulSoup(html_text, "html.parser")
 
-# manages to find the lyrics
-for lyrics in soup.find_all("div", {"class": None, "id": None}):
-    print(lyrics)
+# * Function to extract the lyrics out of an azlyrics webpage
+# ! Doesn't differentiate depending on Artist, need to be implemented
+# ! Maybe use dictionnary to return lyrics so every verse is tied to an artist
+def extract_lyrics(page):
+    html_text = requests.get(page).content
+    soup = BeautifulSoup(html_text, "html.parser")
+    for lyrics in soup.findAll("div", {"class": None, "id": None}):
+        for artist in lyrics.findAll("i"):
+            tag = artist.nextSibling
+            verse = ""
+            while (re.match("(<i>.*</i>)", str(tag)) is None):
+                if (str(tag) != "<br/>"):
+                    verse += str(tag)
+                if (tag.nextSibling is None):
+                    break
+                tag = tag.nextSibling
+            print(verse)
+
+
+# * Just testing if things work as they should
+extract_lyrics(page)
