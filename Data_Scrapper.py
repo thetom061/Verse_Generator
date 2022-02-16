f6d299958e12250605ae7d80f5d8cf26307d9ed1
thetom061 <tcotte061@gmail.com>
2022-02-16 15:16:33 +0100

    Verse extractor works correctly

--- Data_Scrapper.py
+++ Data_Scrapper.py
@@ -5,28 +5,55 @@ import re
 # * Artist Page
 default_page = "https://www.azlyrics.com/w/west.html"
 
-# * Example page
-page = "https://www.azlyrics.com/lyrics/kanyewest/gorgeous.html"
+# * Example page with <i> tags to show who sings verse
+page1 = "https://www.azlyrics.com/lyrics/kanyewest/gorgeous.html"
+
+# * Example page with no <i> tags
+page2 = "https://www.azlyrics.com/lyrics/kanyewest/illflyaway.html"
+
+# * To make things modular we need:
+# * A function that extracts the verses of a page
+# * A function that saves those verses into a txt file
 
 
 # * Function to extract the lyrics out of an azlyrics webpage
-# ! Doesn't differentiate depending on Artist, need to be implemented
-# ! Maybe use dictionnary to return lyrics so every verse is tied to an artist
+# * Returns a Dictionnary containing artists and their Verses
+# ! Only works if page contains an <i> Artist </i> tag
 def extract_lyrics(page):
+    verses = {}
     html_text = requests.get(page).content
     soup = BeautifulSoup(html_text, "html.parser")
+
+    # We grab lyrics
     for lyrics in soup.findAll("div", {"class": None, "id": None}):
+        verse = ""
+        # Loop through artists singing the song
         for artist in lyrics.findAll("i"):
+            # To keep only the names of the singers
+            singers = re.sub("[\[\]:]", '', artist.text)
+            # we want to point to next element aka lyrics
             tag = artist.nextSibling
-            verse = ""
+            # continue looping while the same artist is singing
             while (re.match("(<i>.*</i>)", str(tag)) is None):
                 if (str(tag) != "<br/>"):
                     verse += str(tag)
                 if (tag.nextSibling is None):
                     break
                 tag = tag.nextSibling
-            print(verse)
+            # if the artists already have a verse in the dictionnary, we add the new verse into the list
+            if singers in verses:
+                verses[singers].append(verse)
+            # if singers aren't part of the dictionnary, we add their first verse as a list containing one verse
+            else:
+                verses[singers] = [verse]
+            # reset the verse as
+            verse = ""
+        # if verses is empty singers aren't specified so we just grab the verse and give it an unknown artist
+        if (not verses):
+            verses["Unknown"] = [lyrics.get_text()]
+    return verses
 
 
 # * Just testing if things work as they should
-extract_lyrics(page)
+verses = extract_lyrics(page1)
+print(verses["Kid Cudi"])
